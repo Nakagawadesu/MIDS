57fdda2d00fa906a62dad1d4d0dd9d33c161aa65
Naka <nukitaro@outlook.com>
2022-10-11 21:45:46 -0400

    feat/insertion_sort

--- read_file.c
+++ read_file.c
@@ -1,13 +1,13 @@
 
 
-/* 
+/*
 int swap(Vertex_Degree * vector ,int  a ,int  b)
 {
     Vertex_Degree  aux ;
     aux = vector[a];
     vector[a] = vector[b];
     vector[b] = aux;
-    //printf("%d < - > %d\n",a,b); 
+    //printf("%d < - > %d\n",a,b);
     return 1;
 
 }
@@ -59,175 +59,216 @@ while(end != start )
 
 int Line_max = 256;
 
+typedef struct Adj_Matrix
+{
 
-
-typedef struct Adj_Matrix{
-
-    struct Vertex_Cell * head;
+    struct Vertex_Cell *head;
     int vertex;
     int degree;
-    
-}Adj_Matrix;
 
-typedef struct Vertex_Cell{
+} Adj_Matrix;
+
+typedef struct Vertex_Cell
+{
 
     int adj;
-    struct Vertex_Cell  * next
+    struct Vertex_Cell *next
 
-}Vertex_Cell;
+} Vertex_Cell;
 
-typedef struct Solution{
-    Adj_Matrix * V;
-    int  * D;
-    int * C;
+typedef struct Solution
+{
+    Adj_Matrix *V;
+    int *D;
+    int *C;
     int N_dominants;
     int N_complements;
     int N_vertex;
-}Solution;
+} Solution;
 /* typedef struct Adj_Matrix{
     struct Rows * Row;
     int N_vertices;
 }; */
 
-int print_matrix(Adj_Matrix * Matrix, int N_vertex)
+int print_matrix(Adj_Matrix *Matrix, int N_vertex)
 {
-    Vertex_Cell * aux;
-    
-    for (int i =0 ;i< N_vertex;i++)
-        {
-        if(Matrix[i].head == NULL)
-          {
-              printf("%d : NULL",i + 1);
-              /* for (int j= 0 ; j < N_vertex ; j++)
-              {
-                printf("0 ");
-              } */
-          }
-          else{
-              printf("degree : %d // vertex : %d :",Matrix[i].degree,i + 1);
-              int j = 0 ;
-             aux = Matrix[i].head;
-              while(aux->next != NULL){
+    Vertex_Cell *aux;
 
-                  /* for( j ; j < aux->adj ;j++ )
-                  {
-                      printf("0 ");
-                  }
-                  j = aux->adj;
-                  
-                  printf("1 "); */
-                  printf(" -> %d" , aux->adj);
-                  aux = aux->next;
-              }
-             /*  j = aux->adj;
-              printf("1 "); */
-              printf(" -> %d",aux->adj);
-              /* for(int i = j ; j< N_vertex ; j++)
-              {
-                   printf("0 ");
-              } */
-          }  
-           
-           printf("\n");
+    for (int i = 0; i < N_vertex; i++)
+    {
+        if (Matrix[i].head == NULL)
+        {
+            printf("%d : NULL", i + 1);
+            /* for (int j= 0 ; j < N_vertex ; j++)
+            {
+              printf("0 ");
+            } */
         }
+        else
+        {
+            printf("degree : %d // vertex : %d :", Matrix[i].degree, Matrix[i].vertex);
+            int j = 0;
+            aux = Matrix[i].head;
+            while (aux->next != NULL)
+            {
+
+                /* for( j ; j < aux->adj ;j++ )
+                {
+                    printf("0 ");
+                }
+                j = aux->adj;
+
+                printf("1 "); */
+                printf(" -> %d", aux->adj);
+                aux = aux->next;
+            }
+            /*  j = aux->adj;
+             printf("1 "); */
+            printf(" -> %d", aux->adj);
+            /* for(int i = j ; j< N_vertex ; j++)
+            {
+                 printf("0 ");
+            } */
+        }
+
+        printf("\n");
+    }
 }
 
-
-
-Adj_Matrix * Initialize_Matrix(char * File )
+int print_matrix_degree_only(Adj_Matrix *Matrix, int N_vertex)
 {
-    int N_rows,N_cols,N_ones;
-    int vertex;
-//abre a strnam para o arquivo desejado    
-FILE * file = fopen(File,"r");
+    Vertex_Cell *aux;
 
-if(file == NULL)
+    for (int i = 0; i < N_vertex; i++)
+    {
+        if (Matrix[i].head == NULL)
+        {
+            printf("%d : NULL",Matrix[i].vertex);
+            
+        }
+        else
+        {
+            printf("degree : %d // vertex : %d ", Matrix[i].degree, Matrix[i].vertex);
+            
+        }
+
+        printf("\n");
+    }
+}
+
+Adj_Matrix *Initialize_Matrix(char *File)
+{
+    int N_rows, N_cols, N_ones;
+    int vertex;
+    // abre a strnam para o arquivo desejado
+    FILE *file = fopen(File, "r");
+
+    if (file == NULL)
     {
         printf("NO SUCH FILE\n");
         return 0;
     }
 
+    char *line_buffer = (char *)malloc(sizeof(char) * Line_max);
 
-   char * line_buffer = (char*)malloc( sizeof(char) * Line_max);
-   
-   //ignorando o header e pegando a segunda linha
+    // ignorando o header e pegando a segunda linha
 
-   fscanf(file ,"%*s %*s %*s %*s %*s %d %d %d ",&N_rows ,&N_cols, &N_ones);
-   
+    fscanf(file, "%*s %*s %*s %*s %*s %d %d %d ", &N_rows, &N_cols, &N_ones);
 
-  // printf("%d %d %d\n",N_rows ,N_cols,N_ones);
+    // printf("%d %d %d\n",N_rows ,N_cols,N_ones);
 
-   Adj_Matrix * Matrix = (Adj_Matrix*)malloc(N_rows * sizeof( Adj_Matrix));
-   
-   for(int i = 0 ; i < N_rows; i++ )
-   {
-       Matrix[i].vertex = i; //cada cabeca tera um numero correspondendo ao vertice
-       //para possibilitar a ordenacao por grau no vertice
-   }
+    Adj_Matrix *Matrix = (Adj_Matrix *)malloc(N_rows * sizeof(Adj_Matrix));
 
-   Vertex_Cell * previus = (Vertex_Cell*)malloc(sizeof(Vertex_Cell));
-  
-  
-   for(int i =0 ; i < N_ones + 1 ; i++)
-   {
-       Vertex_Cell * aux = (Vertex_Cell*)malloc(sizeof(Vertex_Cell));
-     
-       fscanf(file,"%d %d",& aux->adj,&vertex);
-       
-     // printf("%d %d ", aux->adj , vertex);
-      //printf("\n");
-       if(Matrix[vertex - 1].head == NULL)
-       {
-           Matrix[vertex - 1].degree++;
-           Matrix[vertex - 1].head = aux;
-           previus = aux;
-       }
-       else{
+    for (int i = 0; i < N_rows; i++)
+    {
+        Matrix[i].vertex = i + 1; // cada cabeca tera um numero correspondendo ao vertice
+        // para possibilitar a ordenacao por grau no vertice
+    }
+
+    Vertex_Cell *previus = (Vertex_Cell *)malloc(sizeof(Vertex_Cell));
+
+    for (int i = 0; i < N_ones + 1; i++)
+    {
+        Vertex_Cell *aux = (Vertex_Cell *)malloc(sizeof(Vertex_Cell));
+
+        fscanf(file, "%d %d", &aux->adj, &vertex);
+
+        // printf("%d %d ", aux->adj , vertex);
+        // printf("\n");
+        if (Matrix[vertex - 1].head == NULL)
+        {
+            Matrix[vertex - 1].degree++;
+            Matrix[vertex - 1].head = aux;
+            previus = aux;
+        }
+        else
+        {
             Matrix[vertex - 1].degree++;
             previus->next = aux;
             previus = aux;
-       }
-       
-      
-       
-   }
-   //print_matrix(Row,N_rows,N_cols);
-   return Matrix;
-
+        }
+    }
+    // print_matrix(Row,N_rows,N_cols);
+    return Matrix;
 }
 
-int Insertion_sort( Adj_Matrix * Matrix , int left , int right )
+int swap(Adj_Matrix *Adj_matrix, int a, int b)
 {
+    Adj_Matrix aux;
 
-    
+    aux = Adj_matrix[a];
+    Adj_matrix[a] = Adj_matrix[b];
+    Adj_matrix[b] = aux;
+   // printf("swapped : %d <-> %d \n", Adj_matrix[a].vertex, Adj_matrix[b].vertex);
+    return 1;
 }
-int  Number_of_adjacency(FILE * File)
+
+int Insertion_sort(Adj_Matrix *Matrix, int left, int right)
 {
-FILE * file = fopen(File,"r");
+    int sorted = left, insert = left + 1;
 
-int n;
+    for (int i = left; i < right; i++)
+    {
+        sorted = insert;
 
-fscanf(file ,"%*s %*s %*s %*s %*s %*d %*d %*d", &n);
+        while (Matrix[sorted].degree < Matrix[sorted - 1].degree )
+        {
 
-//printf("%d",n);
-fclose(file);
+            swap(Matrix, sorted, sorted - 1);
+            sorted--;
+        }
+
+        insert++;
+    }
+    return 1;
+}
+
+int Number_of_adjacency(FILE *File)
+{
+    FILE *file = fopen(File, "r");
+
+    int n;
+
+    fscanf(file, "%*s %*s %*s %*s %*s %*d %*d %*d", &n);
+
+    // printf("%d",n);
+    fclose(file);
     return n;
 }
-int  Number_of_vertex(FILE * File)
+int Number_of_vertex(FILE *File)
 {
-FILE * file = fopen(File,"r");
+    FILE *file = fopen(File, "r");
 
-int n;
+    int n;
 
-fscanf(file ,"%*s %*s %*s %*s %*s %d", &n);
+    fscanf(file, "%*s %*s %*s %*s %*s %d", &n);
 
-//printf("%d",n);
-fclose(file);
+    // printf("%d",n);
+    fclose(file);
     return n;
 }
 
- 
-/* 
+/*
 Solution * Gulosinho(int N_vertex,Vertex_Degree * Degree_Vector ,struct Rows * Adj_matrix)
 {
    Vertex_Degree * Dominant = (Vertex_Degree*)malloc(N_vertex * sizeof(Vertex_Degree));
@@ -239,7 +280,7 @@ Solution * Gulosinho(int N_vertex,Vertex_Degree * Degree_Vector ,struct Rows * A
    {
        if(Adj_matrix[i].Colum != NULL)
        {
-           
+
          printf("dominant vertex: %d \n",D_counter);
          Dominant[D_counter].vertex = Degree_Vector[i].vertex  ;//adiciona o vertice ao conjunto dos dominantes
          D_counter++;
@@ -250,18 +291,18 @@ Solution * Gulosinho(int N_vertex,Vertex_Degree * Degree_Vector ,struct Rows * A
              {
                 printf(" %d ",j);
                 Complement[C_counter].vertex = j+1;//adiciona vertices adjacentes ao conjnto complementar
-                free(Adj_matrix[j].Colum);//remove da lista de adjacencia os vertices 
+                free(Adj_matrix[j].Colum);//remove da lista de adjacencia os vertices
                 C_counter++;
              }
-             
+
          }
          printf("\n");
-        free(Adj_matrix[i].Colum);//remove da matris de adjacencia  G          
+        free(Adj_matrix[i].Colum);//remove da matris de adjacencia  G
        }
        Degree_Vector[i].vertex  = 0;//remove o vertice do conjunto G
-     
 
-   } 
+
+   }
    Solution * solution = (Solution*)malloc(sizeof(Solution));
    //printf(" %d %d",D_counter ,C_counter);
    solution->N_dominants = D_counter;
@@ -272,58 +313,46 @@ Solution * Gulosinho(int N_vertex,Vertex_Degree * Degree_Vector ,struct Rows * A
 
 int main()
 {
-    
- clock_t begin = clock();
 
+    clock_t begin = clock();
 
- char* file_name = "text_matrix";
- //char* file_name = "text_matrix";
- //char* file_name = "frb100-40.txt";
- Adj_Matrix * Matrix = Initialize_Matrix(file_name);
- 
- int N_adjs = Number_of_adjacency(file_name);
- int N_vertex = Number_of_vertex(file_name);
+    //char *file_name = "text_matrix";
+    // char* file_name = "text_matrix";
+     char* file_name = "frb100-40.txt";
+    Adj_Matrix *Matrix = Initialize_Matrix(file_name);
 
- 
- printf("Unsorted\n");
- 
- print_matrix(Matrix,N_vertex);
-/*   for(int i =  0 ;i < N_vertex ;i++)
-{
-    printf("vertex: %d Degree : %d\n",Degree_Vector[i].vertex ,Degree_Vector[i].degree);
-} 
+    int N_adjs = Number_of_adjacency(file_name);
+    int N_vertex = Number_of_vertex(file_name);
 
-//CocktailSort(Degree_Vector,N_vertex);
+    printf("Unsorted\n");
 
-printf("Sorted\n");
-for(int i =  0 ;i < N_vertex ;i++)
-{
-    printf("vertex: %d Degree : %d\n",Degree_Vector[i].vertex,Degree_Vector[i].degree);
-} 
- */
+    print_matrix(Matrix, N_vertex);
 
+    printf("Sorted\n");
+    Insertion_sort(Matrix, 0, N_vertex-1);
+    // swap(Matrix,0,9);
+    print_matrix_degree_only(Matrix, N_vertex);
 
-//Solution * solution = Gulosinho(N_vertex,Degree_Vector , Adj_Matrix);
+    // Solution * solution = Gulosinho(N_vertex,Degree_Vector , Adj_Matrix);
 
-printf("Solution:\n");
+    // printf("Solution:\n");
 
-/* printf("Dominant set:\n");
+    /* printf("Dominant set:\n");
 
-for(int i =  0 ;i < solution->D ;i++)
-{
-    printf(" %d ",solution->D[i].vertex);
-} 
+    for(int i =  0 ;i < solution->D ;i++)
+    {
+        printf(" %d ",solution->D[i].vertex);
+    }
 
-printf("Complement:\n");
+    printf("Complement:\n");
 
-for(int i =  0 ;i < solution->C ;i++)
-{
-    printf(" %d ", solution->C[i].vertex);
-}  */
+    for(int i =  0 ;i < solution->C ;i++)
+    {
+        printf(" %d ", solution->C[i].vertex);
+    }  */
 
-
-clock_t end = clock();
-double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-printf("Executed in : %f s\n", time_spent);
-return 0;
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Executed in : %f s\n", time_spent);
+    return 0;
 }
