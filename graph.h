83f2667e899e72c8c692cd6408f03e41b533a065
Naka <nukitaro@outlook.com>
2023-03-15 23:32:57 -0400

    feat:RCL with doubly linked lists and random choosing with removal

--- graph.h
+++ graph.h
@@ -20,11 +20,13 @@ typedef struct Adj_Matrix{
 typedef struct L_cell{
  int V;
  int D;
- struct L_cell  * next ;
+ struct L_cell * next ;
+ struct L_cell * previus; 
 }L_cell;
 
 typedef struct linked_list{
  struct L_cell * head;
+ struct L_cell * foot;
  int Min_degree;
 }linked_list;
 
@@ -51,6 +53,7 @@ linked_list * create_list()
 {
    linked_list * L = (struct linkedlist*)malloc(sizeof(linked_list));
    L->head = NULL;
+   L->foot = NULL;
    L->Min_degree = 0 ;
    return L;
 }
@@ -59,6 +62,7 @@ L_cell * create_cell(int V , int D)
    L_cell * aux = (L_cell*)malloc(sizeof(L_cell));
    aux->V = V;
    aux->D = D;
+   aux->previus = NULL;
    aux->next = NULL;
    return aux;
 
@@ -68,30 +72,56 @@ int insert_in_list(linked_list * L, int V, int D)
     if(L->head == NULL)
     {
         L->head = create_cell(V,D);
+        L->foot = L->head;
         return 1;
+    }else{
+    L_cell * aux = L->foot;
+    L->foot = create_cell(V,D);
+    aux->next = L->foot;
+    L->foot->previus = aux;
     }
-    L_cell * aux = L->head;
-    while(aux != NULL)
-    {
-        aux = aux->next;
+    
+    return 1;
+}
+
+int remove_Cell_list(linked_list * L, L_cell * current )
+{
+    
+   printf("\nremoved : %d\n",current->V + 1);
+   
+    if(current->V== L->head->V ){
+        L->head = current->next;
+        free(current);
     }
-    aux->next = create_cell(V,D);
+    else if(current->V == L->foot->V){
+        L->foot = current->previus;
+        free(current);
+    }else{
+    L_cell * prev = current->previus;
+    L_cell * next = current->next;
+    next->previus = current->previus;
+    prev->next = current->next;
+    free(current);
+    }
+    
     return 1;
 }
 
 void print_list(linked_list * L)
 {
+    printf("\n");
     if(L->head == NULL)
     {
         return ;
     }
     L_cell * aux = L->head;
-    while(aux != NULL)
+    while(aux->next != NULL)
     {
-        printf("V|D:%d|%d->",aux->V , aux->D);
+        printf("V|D : %d|%d -> ",aux->V + 1 , aux->D);
         aux = aux->next;
+        
     }
-     printf("V|D:%d|%d",aux->V , aux->D);
+     printf("V|D:%d|%d",aux->V + 1 , aux->D);
     return 1;
 }
 
@@ -242,5 +272,37 @@ linked_list * RCL(Adj_Matrix * M ,int max_degree , int n ,float alfa )
  }
  return RCL;
 }   
+int Choose_Random_RCL(linked_list * RCL )
+{
+    
+    if (RCL->head == NULL)
+       return -1;
+ 
+    srand(time(NULL));
+ 
+    int selected = RCL->head->V;
+    L_cell * To_remove ;
+    To_remove = RCL->head;
+    
+    L_cell  * current = RCL->head;
+    
+    for (int n=2; current!=NULL; n++)
+    {
+        
+        if (rand() % n == 0)
+        {
+           selected = current->V;
+           To_remove = current;
+          // printf("\nto remove : %d\n",To_remove->V + 1);
+        }
+           
+        current = current->next;
+
+    }
+    
+    //printf("\nRandomly selected key is %d\n", selected+1);
+    remove_Cell_list(RCL,To_remove);
+    return selected;
+}
 
 #endif // GRAPH_H
